f99e73b82a4dec5a2ab55f3146599a66a7c69d62
Chris Schade <chris.schade@gmail.com>
2024-06-12 22:13:41 +0200

    Basic Ratatui layout, message overlaping border

--- yazi-fm/src/confirm/confirm.rs
+++ yazi-fm/src/confirm/confirm.rs
@@ -1,6 +1,7 @@
-use ratatui::{buffer::Buffer, layout::{Constraint, Layout, Rect}, text::{Line, Text}, widgets::{Block, BorderType, Paragraph, Widget}};
+use ratatui::{buffer::Buffer, layout::{Constraint, Direction, Layout, Rect}, text::{Line, Text}, widgets::{Block, BorderType, Borders, Paragraph, Widget}};
 use yazi_config::THEME;
 
+// use yazi_plugin::elements::Paragraph;
 use crate::Ctx;
 
 pub(crate) struct Confirm<'a> {
@@ -10,29 +11,32 @@ pub(crate) struct Confirm<'a> {
 impl<'a> Confirm<'a> {
 	pub(crate) fn new(cx: &'a Ctx) -> Self { Self { cx } }
 }
-
 impl<'a> Widget for Confirm<'a> {
 	fn render(self, _win: Rect, buf: &mut Buffer) {
 		let confirm = &self.cx.confirm;
 		let area = self.cx.area(&confirm.position);
 
 		yazi_plugin::elements::Clear::default().render(area, buf);
+		let outer_layout = Layout::default()
+			.direction(Direction::Vertical)
+			.constraints(vec![Constraint::Percentage(70), Constraint::Percentage(30)])
+			.split(area);
+
+		let inner_layout = Layout::default()
+			.direction(Direction::Horizontal)
+			.constraints(vec![Constraint::Percentage(50), Constraint::Percentage(50)])
+			.split(outer_layout[1]);
+
+		Block::bordered()
+			.border_type(BorderType::Rounded)
+			.border_style(THEME.input.border)
+			.title(Line::styled(&confirm.title(), THEME.input.title))
+			.render(area, buf);
 
 		Paragraph::new(confirm.message().split('\n').map(Line::from).collect::<Vec<Line>>())
-			.block(
-				Block::bordered()
-					.border_type(BorderType::Rounded)
-					.border_style(THEME.input.border)
-					.title(Line::styled(&confirm.title(), THEME.input.title)),
-			)
-			.render(chunks[0], buf);
+			.render(outer_layout[0], buf);
 
-		let text = Text::from(vec![
-			Line::from("hello world 1"),
-			Line::from("hello world 2"),
-			Line::from("hello world 3"),
-		])
-		.centered();
-		Paragraph::new(text).block(Block::bordered()).render(chunks[1], buf);
+		Paragraph::new("[Y]es").block(Block::bordered()).render(inner_layout[0], buf);
+		Paragraph::new("(N)o").block(Block::bordered()).render(inner_layout[1], buf);
 	}
 }
